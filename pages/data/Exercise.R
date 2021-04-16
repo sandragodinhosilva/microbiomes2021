9c36784718670519faca1b19ae7214b85da66fdc
Sandra G. Silva <sandragodinhosilva@gmail.com>
2021-04-16 10:44:30 +0100

    update

--- pages/data/Exercise.R
+++ pages/data/Exercise.R
@@ -10,4 +10,27 @@
 library("dplyr")
 
 ################################################################################
-# Exercise 1: Open "cog_output.file"
+# Open cog_table.csv file"
+cog_table = read.csv("cog_table.csv")
+head(cog_table)
+
+remove <- c("Accession", "Name", "Descriptiom")
+samples = colnames(cog_table)
+cog_table[ , !names(cog_table) %in% remove] ## works as expected
+
+
+# Open mapping file
+cog_map = read.table("cog_mapping.tsv", sep="\t", header=TRUE)
+head(cog_map)
+
+cog_map2 = read.table("cog_mapping2.tsv", sep="\t", header=TRUE)
+head(cog_map2)
+
+merge_table = merge(cog_table, cog_map2, by.x="Accession", by.y="COG")
+head(merge_table)
+
+merge_table %>% 
+  count(func)
+  
+  group_by(func)  %>%
+  count()
